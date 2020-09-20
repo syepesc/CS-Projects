e522f51b5da4427bcfc193f8bd8358fc0436c843
Santiago Yepes <64435277+syepesc@users.noreply.github.com>
2020-09-20 19:07:13 -0400

    Add files via upload

---
+++ OperatorOverlaod/Complex.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace ClassExercise_Complex
+{
+    public class Complex
+    {
+        public int Real { get; }
+        public int Imaginary { get; }
+        public double Argument { get; }
+        public double Modulus { get; }
+        public static Complex zero;
+        public Complex(int real, int imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+            Modulus = Math.Sqrt(real * real + imaginary * imaginary);
+            Argument = Math.Atan(imaginary / real);
+        }
+        public Complex Zero
+        {
+            get
+            {
+                return new Complex(0, 0);
+            }
+        }
+        public override string ToString()
+        {
+            return $"[({Real}), ({Imaginary})]";
+        }
+        // con este metodo se pueden sumar, restar, multiplar dividir o diferencia objetos!!!!!!!!!
+        public static Complex operator +(Complex lhs, Complex rhs)
+        {
+            int real = lhs.Real + rhs.Real;
+            int imaginary = lhs.Imaginary + rhs.Imaginary;
+            return new Complex(real, imaginary);
+        }
+        public static Complex operator -(Complex lhs, Complex rhs)
+        {
+            int real = lhs.Real - rhs.Real;
+            int imaginary = lhs.Imaginary - rhs.Imaginary;
+            return new Complex(real, imaginary);
+        }
+        public static bool operator ==(Complex lhs, Complex rhs)
+        {
+            if (lhs.Real == rhs.Real && lhs.Imaginary == rhs.Imaginary) 
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Complex lhs, Complex rhs)
+        {
+            if (lhs.Real == rhs.Real && lhs.Imaginary == rhs.Imaginary)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static Complex operator *(Complex lhs, Complex rhs)
+        {
+            int real = lhs.Real * rhs.Real - lhs.Imaginary * rhs.Imaginary;
+            int imaginary = lhs.Real * rhs.Imaginary + lhs.Imaginary * rhs.Real;
+            return new Complex(real, imaginary);
+        }
+        public static Complex operator -(Complex complex)
+        {
+            int real = complex.Real * -1;
+            int imaginary = complex.Imaginary * -1;
+            return new Complex(real, imaginary);
+        }
+    }
+}
+
---
+++ OperatorOverlaod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassExercise_Complex
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Complex c0 = new Complex(-2, 3);
+            Complex c1 = new Complex(-2, 3);
+            Complex c2 = new Complex(1, -2);
+
+            Console.WriteLine(c0);
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+            Console.WriteLine($"{c1} + {c2} = {c1 + c2}");
+            Console.WriteLine($"{c1} - {c2} = {c1 - c2}");
+
+            Complex c3 = c1 + c2;
+
+            Console.WriteLine($"{c3} in polar form is {c3.Modulus:f2} cis ({c3.Argument:f2})");
+
+            Console.WriteLine($"{c0} {(c0 == c1 ? "=" : "!=")} {c1}");
+            Console.WriteLine($"{c0} {(c0 == c2 ? "=" : "not equal")} {c2}");
+
+            // Additional tasks
+            Console.WriteLine($"{c1} * {c2} = {c1 * c2}");
+            Console.WriteLine($"-{c1} = {-c1}");
+            Console.ReadLine();
+        }
+    }
+}
